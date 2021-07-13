40113ee2c18311be8fec13064b7cbace3363334e
SIVANESH-P <87260045+SIVANESH-P@users.noreply.github.com>
2021-07-13 20:15:49 +0530

    Add files via upload
    I tried arithmetic operators

--- Hello.java
+++ Hello.java
@@ -3,37 +3,52 @@ import java.util.Scanner;
 public class Hello {
 
 	public static void main(String[] args) {
-    System.out.println("Hello java this is siva");
-    
-    
-    int x = 2;
-    System.out.println(x);
-    float decimal =(float)11.4;
-    System.out.println(decimal);
-    char my_char = 's';
-    System.out.println(my_char);
-    boolean is_false = true;
-    System.out.println(is_false);
    
-    
-    Scanner scan = new Scanner (System.in);
-    System.out.println("Enter your favourite number");
-    int user_favourite_number = scan.nextInt();
-    System.out.println("Your entered number is");
-    System.out.print(user_favourite_number);
-    
-    Scanner scan1 = new Scanner (System.in);
-    System.out.println("Enter some decimal value");
-    double user_input_double = scan1.nextDouble();
-    System.out.println("The entered value is");
-    System.out.println(user_input_double);
-    
-    Scanner word = new Scanner(System.in);
-    System.out.println("Enter username");
-    String user_name = word.nextLine();
-    System.out.println("Your username is");
-    System.out.print(user_name);
-       
+		// Today I gonna try some Arithmetic Operators
+		// First I'll try addition of two numbers
+		
+		int a , b , answer;
+		a = 25;
+		b = 189;
+		answer = a + b;
+		
+		System.out.println("Your answer = " + answer);
+		
+		//Now I'll try subtraction of two numbers
+		
+		int x , y , Answer;
+		x = 53;
+	    y = 64;
+	    Answer = x - y;
+	    	
+	    
+	    System.out.println("Your answer = " + Answer );
+	    
+	    //Now, multiplication of two decimal numbers
+	    
+	    double p , q , r;
+	    p = 12.3;
+	    q = 14.8;
+	    r = p * q;
+	    System.out.println("answer = " + r);
+	    
+	    //Now, division and finding the quotient
+	    
+	    double c , d , e;
+	    c = 125;
+	    d = 3;
+	    e = 125 / 3;
+	    System.out.println("Quotient = " + e);
+	    
+	    //Now, division and finding the remainder
+	    
+	    double m , n , o;
+	    m = 67;
+	    n = 5;
+	    o = 67 % 5;
+	    System.out.println("Remainder = " + o);
+	    
+	    System.out.println("I'm done with the operators!");
 	}
 
 }
