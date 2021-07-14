62998d3783c8f54b651bf36bca9daea3bb9475ad
SIVANESH-P <87260045+SIVANESH-P@users.noreply.github.com>
2021-07-14 13:49:25 +0530

    Add files via upload

---
+++ project3.java
@@ -0,0 +1,36 @@
+
+public class project3 {
+
+	public static void main(String[] args) {
+		
+		//Increment operator and Assignment operator
+		
+		int x = 43;
+		x = x + 1;
+		System.out.println(x);
+		
+		//Post increment operator
+		int y = 52;
+		System.out.println(y--);
+		System.out.println(y);
+		
+		//Pre increment operator
+		int z = 6;
+		System.out.println(++z);
+		System.out.println(z);
+		
+		//Assignment operator
+		int a = 74;
+		a *= 5;
+		System.out.println(a);
+		
+		double b = 63;
+		b /= 2;
+		System.out.println(b);
+		
+		double c = 77.83;
+		c -= 63.75;
+		System.out.println(c);
+	}
+
+}
