5ecd1498988826140986a6303e22813b4ad4512f
Sivanesh <87260045+SIVANESH-P@users.noreply.github.com>
2023-04-10 16:33:25 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,42 @@
+public class Main {
+    public static void main(String[] args){
+       int x; //declaration
+       x = 25; //assignment
+       System.out.println(x);
+
+       int y = 125; //initialization
+       System.out.println(y);
+
+       String name = "Sivanesh";
+       System.out.println("My name is "+ name);
+
+       Boolean a = true;
+       System.out.println(a);
+
+       Byte boys = 127;
+       System.out.println(boys);
+
+       char symbol = 's';
+       System.out.println(symbol);
+
+       //Swapping two variables
+
+       String z = "Orange juice";
+       String q = "Mango juice";
+
+       System.out.println("z: "+z);
+       System.out.println("q: "+q);
+
+       //After swapping
+       String temp;
+       temp=z;
+       z=q;
+       q=temp;
+       System.out.println("After swapping z "+z);
+       System.out.println("After swapping q: "+q);
+
+
+
+    }
+   
+}
